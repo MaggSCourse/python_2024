101c0c0b05732b74c585f0041207ba783523f5fa
MaggSCourse <sahesi86@gmail.com>
2024-01-23 20:32:53 -0600

    Adding examples of dictionaries, tuples and sets, besides a brief description of each one

---
+++ 012324.002.tuples.py
@@ -0,0 +1,11 @@
+'''
+002. Tuplas
+- las tuplas se usan con ()
+- las tuplas son indexadas e inician en el 0
+- no permite modificar o asignar
+- no se pueden agregar diccionarios
+- se pueden agregar listas
+'''
+
+my_tuple = (1,2,3,4,5,6)
+print(my_tuple)
---
+++ 012324.003.sets.py
@@ -0,0 +1,9 @@
+'''
+003 Sets
+- los sets se forman usando { }
+- los sets no son indexables
+- no permite duplicados
+- permite int,str, boolean, float, es decir guarda cualquier tipo de variable
+
+
+'''
