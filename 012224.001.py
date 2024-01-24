e8cbe44307b242b0c06283d9b7d940a22309cbe8
MaggSCourse <sahesi86@gmail.com>
2024-01-23 21:51:32 -0600

    Learning list and methods

--- 012224.001.py
+++ 012224.001.py
@@ -1,10 +1,41 @@
 '''
-
-
+001 List
+- 
 
 '''
 
+list_my_students = [
+    'Maggie',   #0
+    'Ibrahim',  #1
+    'Paulina',  #2
+    'Octavio',  #3
+    'Ulises' ,  #4
+    2024,       #5
+    2.50        #6
+    ]   
+'''
+print(list_my_students)
+print(list_my_students[0])
+print(list_my_students[1])
+print(list_my_students[2])
+print(list_my_students[3])
+print(list_my_students[4])
+list_my_students.append("Luis")
+list_my_students.insert(0,'Isela')
+list_my_students.extend(['Javier', 'Isela', 'Limon'])
+'''
 
+#print(list_my_students)
+#remove elements
+#list_my_students.pop()
+#list_my_students.pop(1)
+#list_my_students.remove("Paulina")
+#list_my_students.clear()
+#print(len(list_my_students))
+#print(list_my_students)
+
+for student in list_my_students : 
+    print(student)
 
 
 
