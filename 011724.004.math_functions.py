89111862e54517963d2b3a4f6d53ccc417996b7c
MaggSCourse <sahesi86@gmail.com>
2024-01-23 19:14:16 -0600

    Fixing branches

---
+++ 011724.004.math_functions.py
@@ -0,0 +1,11 @@
+'''
+004 Math functions
+Las funciones matematicas, son funciones que ya existen en un modulo y podemos usar sin programarlasnosotros mismos
+
+'''
+
+print(min(12,1,5,19)) #te imprime el numero con menor valor
+print(max(33,56,90,.5)) # te imprime el numero con mayor valor
+print(abs(-3.123)) #el valor absoluto siempre es un numero positivo
+print(pow(3,3)) # exponencial de un numero
+print(round(3.1416192385, 2)) #el numero despues de la coma son los decimales que tomara en cuenta
