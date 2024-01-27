5709af9725c79382db03bc1e55f1d8bf70a15887
MaggSCourse <sahesi86@gmail.com>
2024-01-26 21:08:48 -0600

    duplicated plus exercises ww04

---
+++ 012524_Ejercicios_WW04/012624_ejercicio001.py
@@ -0,0 +1,8 @@
+'''
+Bucles . Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad)
+'''
+edad = 1
+edad = int(input('Introduce tu edad: '))
+for anios in edad:
+    print('Tu edad desde 1 hasta ahora es: ')
+
